3ffc242b7021c2cf4e3f66fd305cb2cb5ac91816
penguyen <peternguyen008@gmail.com>
2016-02-27 14:48:00 -0500

    seeding seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -3,3 +3,97 @@ INSERT INTO brand (name, headquarters) VALUES ('Jordan', 'Beaverton, Oregon, USA
 INSERT INTO brand (name, headquarters) VALUES ('Nike', 'Beaverton, Oregon, USA');
 INSERT INTO brand (name, headquarters) VALUES ('Adidas', 'Herzogenaurach, Germany');
 
+-- sneakers
+        --Jordan
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) 
+    VALUES('Jordan 1 - Banned Bred', 1, 140, 2000, 'One of the first original Jordans worn by Michael Jordan, and the first banned shoe in NBA history for violating the NBA\s uniformity of uniform rule.');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) 
+    VALUES('Jordan 1 - Royal', 1, 140, 2000,'One of the first original Jordans worn by Michael Jordan. Released in 1985 during the advent of Michael Jordan\s NBA career.');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) 
+    VALUES('Jordan 1 - Chicago', 1, 140, 900,'One of the first original Jordans worn by Michael Jordan during the majority of his 1985 rookie season.');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) 
+    VALUES('Jordan 1 - Shattered Blackboard', 1, 140, 700,'Created in remembrance of the time Michael Jordan dunked and shattered the blackboard during an off-season exhibition game.');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) 
+    VALUES('Jordan 1 - Fragment', 1, 140, 1500,'Designed by legendary streetwear designer and influencer, Hiroshi Fujiwara as a tribute to Michael Jordan\s legacy.');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) 
+    VALUES('Jordan 2 - Italian Stalion', 1, 175, 500,'Worn by Michael Jordan when he won the 1987 NBA All Star Slam Dunk Contest.');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) 
+    VALUES('Jordan 2 - Just Don', 1, 300, 1500,'Limited edition collaboration between designer Don Crawley (aka Don C) and Jordan Brand.');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) 
+    VALUES('Jordan 3 - White Cement', 1, 150, 500,'Worn by Michael Jordan when he won the 1988 NBA All Star Slam Dunk Contest.');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) 
+    VALUES('Jordan 3 - Black Cement', 1, 150, 500,'Worn by the MVP of the 1988 NBA All Star Game.');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) 
+    VALUES('Jordan 4 - Bred', 1, 150, 350,'Worn by Michael Jordan during the 1989 NBA All Star Game.');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) 
+    VALUES('Jordan 5 - Supremes', 1, 190, 650,'Exclusive limited edition design in collaboration with legendary skate shop, Supreme.');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) 
+    VALUES('Jordan 5 - Metallic', 1, 150, 300,'Worn by Michael Jordan during the 1990 NBA All Star Game.');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) 
+    VALUES('Jordan 6 - Black Infrared', 1, 150, 300,'Worn by Michael Jordan during the 1991 NBA All Star Game.');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) 
+    VALUES('Jordan 7 - Olympics', 1, 150, 450,'An original work of art worn by Dream Team gold medalist, Michael Jordan during the 1992 Summer Olympics.');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) 
+    VALUES('Jordan 7 - Bordeaux', 1, 150, ,'Worn by Michael Jordan during the 1992 NBA All Star Game.');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) 
+    VALUES('Jordan 8 - Kobes', 1, 190, 600,'This pair was NOT worn by Michael Jordan, but rather was created as a tribute in honor of the retirement of NBA basketball legend, Kobe Bryant.');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) 
+    VALUES('Jordan 11 - Space Jam', 1, 150, 450,'Created and released in 1996 as an exclusive pair for Michael Jordan for his movie, Space Jam.');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) 
+    VALUES('Jordan 11 - Columbia', 1, 175, 400,'Worn by the MVP of the 1996 NBA All Star Game. The colorway was influenced in honor of Jordan\s college, University of North Carolina.');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) 
+    VALUES('Jordan 11 - 72-10', 1, 190, 300,'Created and designed in honor of Michael Jordan and the Chicago Bull\s near-perfect season and current NBA record of 72 wins and 10 losses during 1995-96.');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) 
+    VALUES('Jordan 12 - Flu Game', 1, 150, 250,'In remembrance of MVP Michael Jordan and the Chicago Bulls winning the 1997 NBA Championships while also battling the flu.');
+        -- Nike
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+        -- Adidas
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+INSERT INTO sneakers(name, brand_id, retail_price, resale_price, description) VALUES('Jordan', , , '');
+
+
+
+
+
+
+
+
+
+
